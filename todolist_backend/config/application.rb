3fc9abd77f5e909607ee7d100dce33c4a634fee1
Ahmed Elsherif <ahmed.elsherif.cs@gmail.com>
2018-03-10 15:21:47 +0200

    [Backend] Authentication login, signup and show profile

--- todolist_backend/config/application.rb
+++ todolist_backend/config/application.rb
@@ -11,5 +11,11 @@ module TodolistBackend
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths << Rails.root.join('lib')
+    config.middleware.use ActionDispatch::Flash
+     # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
   end
 end
