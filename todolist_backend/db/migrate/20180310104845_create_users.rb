c281f156ab13c35e53dd7bb4e9d16729c11544ba
Ahmed Elsherif <ahmed.elsherif.cs@gmail.com>
2018-03-10 13:08:58 +0200

    [Backend] Create the database and User model

---
+++ todolist_backend/db/migrate/20180310104845_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def up
+    create_table :users do |t|
+      t.string :name, :null => false
+      t.string :email, :null => false, :unique => true, :index => true
+      t.string :password_digest, :null => false
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :users
+  end
+end
