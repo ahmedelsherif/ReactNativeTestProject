c3c38a4728692773fb7e79a5b34e9cbc1a2ee48d
Ahmed Elsherif <ahmed.elsherif.cs@gmail.com>
2018-03-12 23:50:08 +0200

    [React Native] fix keyboard issue and read email and password from the fields in the button action method

--- TodoListApp/src/screens/loginScreen/loginScreen.js
+++ TodoListApp/src/screens/loginScreen/loginScreen.js
@@ -1,28 +1,39 @@
 import React, {Component} from 'react';
 import styles from "./loginStyle"
-import {
-    Text,
-    TextInput,
-    Button,
-    View
-} from 'react-native';
+import {Platform, Text, TextInput, Button, View, KeyboardAvoidingView} from 'react-native';
 import Reactotron from 'reactotron-react-native'
 
 
 type Props = {};
 export default class loginScreen extends Component<Props> {
+
+    constructor(props) {
+        super(props);
+        this.state = { email: '', password: '' };
+        this.loginButtonTouched = this.loginButtonTouched.bind(this);
+    }
+
     render() {
         Reactotron.log('login screen render');
         return (
-            <View style={styles.container}>
+            <KeyboardAvoidingView behavior={'padding'} style={styles.container}
+                 keyboardVerticalOffset={
+                     Platform.select({
+                        ios: () => 0,
+                         android: () => -60
+                     })()
+                 }>
                 <Text style={styles.title}>
                     Login
                 </Text>
                 <TextInput
+                    ref='emailInput'
                     style={styles.textInput}
                     placeholder="Email"
-                    autoFocus = {true}
+                    keyboardType= 'email-address'
+                    // autoFocus = {true}
                     returnKeyType = {"next"}
+                    onChangeText={(val) => {this.setState({email: val})}}
                     onSubmitEditing={(event) => {
                         this.refs.passwordInput.focus();
                     }}
@@ -32,6 +43,8 @@ export default class loginScreen extends Component<Props> {
                     style={styles.textInput}
                     placeholder="password"
                     returnKeyType = {"done"}
+                    secureTextEntry
+                    onChangeText={(val) => {this.setState({password: val})}}
                 />
                 <View style={styles.login}>
                     <Button
@@ -39,12 +52,12 @@ export default class loginScreen extends Component<Props> {
                         onPress={this.loginButtonTouched}
                     />
                 </View>
-            </View>
+            </KeyboardAvoidingView>
         );
     }
 
     loginButtonTouched(){
-        Reactotron.log('login button touched');
+        Reactotron.log('login button touched, email: '+this.state.email+'  password: '+this.state.password);
     }
 }
 
